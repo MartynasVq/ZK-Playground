cb1c5286dc43a2808140fd4345691e2415b18953
Martynas <nbtsst@gmail.com>
2021-05-27 01:54:08 +0300

    Init

---
+++ src/main/java/com/martynasvq/zkstarter/ZkstarterApplication.java
@@ -0,0 +1,13 @@
+package com.martynasvq.zkstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkstarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkstarterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/martynasvq/zkstarter/controllers/IndexController.java
@@ -0,0 +1,8 @@
+package com.martynasvq.zkstarter.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+}
